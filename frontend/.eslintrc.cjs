3c83515f9837e9a6f11d4b7dfb368d1b2da769f6
mrpinkcat <gatien.anizan@gmail.com>
2023-03-28 18:42:09 +0200

    style(linter): add object & array new line rule

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -8,23 +8,56 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
+  parserOptions: { ecmaVersion: 'latest' },
   rules: {
     'vue/multi-word-component-names': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
+    'comma-dangle': [ 'error', 'always-multiline' ],
     'no-console': 'warn',
-    'vue/block-tag-newline': ['error', {
-      singleline: 'never',
-      multiline: 'always',
-      maxEmptyLines: 1,
-    }],
-    'vue/component-name-in-template-casing': ['error', 'kebab-case', { registeredComponentsOnly: false }],
-    'vue/custom-event-name-casing': ['error', 'kebab-case'],
+    'vue/block-tag-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always',
+        maxEmptyLines: 1,
+      },
+    ],
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      { registeredComponentsOnly: false },
+    ],
+    'vue/custom-event-name-casing': [ 'error', 'kebab-case' ],
     'vue/require-name-property': 'error',
-    'vue/component-tags-order': ['error', {
-      order: ['template', 'script', 'style'],
-    }],
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: [
+          'template',
+          'script',
+          'style',
+        ], 
+      },
+    ],
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 },
+    ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-newline': [
+      'error',
+      {
+        'multiline': true,
+        'minItems': 3, 
+      },
+    ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'array-element-newline': [
+      'error',
+      {
+        'multiline': true,
+        'minItems': 3, 
+      },
+    ],
   },
 }
