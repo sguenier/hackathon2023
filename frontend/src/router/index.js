bf9e52225b0c144c1c9ecd4c520d81886a5ce6b8
mrpinkcat <gatien.anizan@gmail.com>
2023-03-29 01:32:39 +0200

    feat(register): add register

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,11 @@ const router = createRouter({
       name: 'login',
       component: () => import('@/views/Login.vue'),
     },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Register.vue'),
+    },
   ],
 })
 
