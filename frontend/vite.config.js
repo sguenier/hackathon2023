0fd02615a16bc463459b8bdd4547d343d9e82c7f
mrpinkcat <gatien.anizan@gmail.com>
2023-03-29 01:28:20 +0200

    style: lint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import {
   fileURLToPath,
-  URL
+  URL,
 } from 'node:url';
 
 import { defineConfig } from 'vite';
@@ -28,12 +28,10 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    vue()
-  ],
+  plugins: [ vue() ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
