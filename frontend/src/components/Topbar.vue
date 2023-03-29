3eeca785fef8191eb699d036b8485f2398032cea
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-03-29 18:42:36 +0200

    feat(topbar): change color and add shadow

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -58,12 +58,16 @@ export default {
 
 <style lang="scss" scoped>
 .topbar {
-  background-color: var(--color-background);
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
+  border-width: 0px 1px 1px 1px;
+  border-style: solid;
+  border-color: var(--color-border);
+  box-shadow: 0px 0px 2px rgba(23, 43, 77, 0.04), 0px 3px 2px rgba(23, 43, 77, 0.08);
+  border-radius: 3px;
 
   &__menu {
     color: var(--text-secondary);
