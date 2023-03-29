aa181b21345f8b8ca9058f25673221abd8619317
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-03-29 15:33:23 +0200

    feat(top-bar): structure and css done

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="topbar">
     <font-awesome-icon icon="bars" class="topbar__menu" />
-    <img class="topbar__logo" src="@/assets/logo.svg" alt="Jajic" />
-    <div>
+    <router-link to="/">
+      <img class="topbar__logo" src="@/assets/logo.svg" alt="Jajic" />
+    </router-link>
+    <div class="topbar__container">
       <font-awesome-icon icon="magnifying-glass" class="topbar__search" />
-
+      <div class="topbar__avatar" ></div>
     </div>
   </header>
 </template>
@@ -20,7 +22,6 @@ export default {
 <style lang="scss" scoped>
 .topbar {
   background-color: var(--color-background);
-  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -28,11 +29,42 @@ export default {
   padding: 1rem;
 
   &__menu {
-
+    color: var(--text-secondary);
   }
+
+  &__menu:hover {
+    cursor: pointer;
+    color: var(--text-primary);
+  }
+
   &__logo {
     max-height: 2.5rem;
   }
+
+  &__container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__avatar {
+    width: 2rem;
+    height: 2rem;
+    background-color: var(--color-border);
+    border-radius: 1rem;
+  }
+
+  &__search {
+    color: var(--text-secondary);
+    margin-right: 1rem;
+  }
+
+  &__search:hover {
+    cursor: pointer;
+    color: var(--text-primary);
+  }
+  
 }
 
 </style>
