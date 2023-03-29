294be0eb59847bb704c07132cda9fda7ca478052
Yohan Centanni <thorbenliya@gmail.com>
2023-03-29 22:00:30 +0200

    commit tag api

--- backend/src/Controller/Back/TagController.php
+++ backend/src/Controller/Back/TagController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Back;
 use App\Entity\Tag;
 use App\Form\TagType;
 use App\Repository\TagRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/tag')]
 class TagController extends AbstractController
@@ -16,54 +18,59 @@ class TagController extends AbstractController
     #[Route('/', name: 'app_tag_index', methods: ['GET'])]
     public function index(TagRepository $tagRepository): Response
     {
-        return $this->render('tag/index.html.twig', [
-            'tags' => $tagRepository->findAll(),
-        ]);
+        return $this->json($tagRepository->findAll());
+
     }
 
-    #[Route('/new', name: 'app_tag_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, TagRepository $tagRepository): Response
+    #[Route('/new/', name: 'app_tag_new', methods: ['POST'])]
+    public function new(Request $request, TagRepository $tagRepository, UserRepository $userRepository): Response
     {
+      
+        $data = json_decode($request->getContent(), true);
         $tag = new Tag();
-        $form = $this->createForm(TagType::class, $tag);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $tagRepository->save($tag, true);
-
-            return $this->redirectToRoute('app_tag_index', [], Response::HTTP_SEE_OTHER);
+        if (!isset($data['name'])) {
+            return new JsonResponse(['error' => 'Missing required fields'], 400);
         }
 
-        return $this->renderForm('tag/new.html.twig', [
-            'tag' => $tag,
-            'form' => $form,
-        ]);
+        if(!$request->headers->has('Authorization')) {
+            return new JsonResponse(['error' => 'Missing authorization header'], 400);
+        }else{
+            $token = $request->headers->get('Authorization');
+            $token = str_replace('Basic ', '', $token);
+            $user = $userRepository->findOneBy(['session_token' => $token]);
+            if($user == null) {
+                return new JsonResponse(['error' => 'Invalid token'], 400);
+            }
+        }
+
+        $tag->setName($data['name']);
+
+        // check if there is a tag with the same name
+        $tagExists = $tagRepository->findOneBy(['name' => $tag->getName()]);
+        if($tagExists != null) {
+            return new JsonResponse(['error' => 'Tag already exists'], 400);
+        }
+
+        $tagRepository->save($tag, true);
+        if($tag->getId() == null) {
+            return new JsonResponse(['error' => 'Tag not created'], 400);
+        }else{
+            return new JsonResponse(['success' => 'Tag created'], 200);
+        }
+
     }
 
-    #[Route('/{id}', name: 'app_tag_show', methods: ['GET'])]
+    #[Route('/{id}/', name: 'app_tag_show', methods: ['GET'])]
     public function show(Tag $tag): Response
     {
-        return $this->render('tag/show.html.twig', [
-            'tag' => $tag,
-        ]);
+       $jsonedTag = $this->json($tag);
     }
 
     #[Route('/{id}/edit', name: 'app_tag_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Tag $tag, TagRepository $tagRepository): Response
     {
-        $form = $this->createForm(TagType::class, $tag);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $tagRepository->save($tag, true);
-
-            return $this->redirectToRoute('app_tag_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('tag/edit.html.twig', [
-            'tag' => $tag,
-            'form' => $form,
-        ]);
+      
     }
 
     #[Route('/{id}', name: 'app_tag_delete', methods: ['POST'])]
