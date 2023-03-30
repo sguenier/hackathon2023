56f1ce0de5c0b15274daec53b91085c0c82ce5f5
Yohan Centanni <thorbenliya@gmail.com>
2023-03-30 11:28:43 +0200

    api endpoints for tags

--- backend/src/Controller/Back/TagController.php
+++ backend/src/Controller/Back/TagController.php
@@ -64,20 +64,68 @@ class TagController extends AbstractController
     #[Route('/{id}/', name: 'app_tag_show', methods: ['GET'])]
     public function show(Tag $tag): Response
     {
-       $jsonedTag = $this->json($tag);
+       $jsonedTag = [
+           'id' => $tag->getId(),
+           'name' => $tag->getName(),
+       ];
+       return new JsonResponse($jsonedTag, 200);
     }
 
-    #[Route('/{id}/edit', name: 'app_tag_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Tag $tag, TagRepository $tagRepository): Response
+    #[Route('/{id}/edit/', name: 'app_tag_edit', methods: ['PUT'])]
+    public function edit(Request $request, Tag $tag, TagRepository $tagRepository, UserRepository $userRepository): Response
     {
-      
+       $data = json_decode($request->getContent(), true);
+
+         if (!isset($data['name'])) {
+              return new JsonResponse(['error' => 'Missing required fields'], 400);
+         }
+
+         if(!$request->headers->has('Authorization')) {
+              return new JsonResponse(['error' => 'Missing authorization header'], 400);
+            }else{
+                $token = $request->headers->get('Authorization');
+                $token = str_replace('Basic ', '', $token);
+                $user = $userRepository->findOneBy(['session_token' => $token]);
+                if($user == null) {
+                    return new JsonResponse(['error' => 'Invalid token'], 400);
+                }
+            }
+
+            // check if tag with the same name already exists
+            $tagExists = $tagRepository->findOneBy(['name' => $data['name']]);
+            if($tagExists != null) {
+                return new JsonResponse(['error' => 'Tag already exists'], 400);
+            }
+
+            $tag->setName($data['name']);
+            $tagRepository->save($tag, true);
+            if($tag->getId() == null) {
+                return new JsonResponse(['error' => 'Tag not updated'], 400);
+            }else{
+                return new JsonResponse(['success' => 'Tag updated'], 200);
+            }
+
     }
 
-    #[Route('/{id}', name: 'app_tag_delete', methods: ['POST'])]
-    public function delete(Request $request, Tag $tag, TagRepository $tagRepository): Response
+    #[Route('/{id}/', name: 'app_tag_delete', methods: ['DELETE'])]
+    public function delete(Request $request, Tag $tag, TagRepository $tagRepository, UserRepository $userRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$tag->getId(), $request->request->get('_token'))) {
+        if(!$request->headers->has('Authorization')) {
+            return new JsonResponse(['error' => 'Missing authorization header'], 400);
+        }else{
+            $token = $request->headers->get('Authorization');
+            $token = str_replace('Basic ', '', $token);
+            $user = $userRepository->findOneBy(['session_token' => $token]);
+            if($user == null) {
+                return new JsonResponse(['error' => 'Invalid token'], 400);
+            }
+
             $tagRepository->remove($tag, true);
+            if($tag->getId() != null) {
+                return new JsonResponse(['error' => 'Tag not deleted'], 400);
+            }else{
+                return new JsonResponse(['success' => 'Tag deleted'], 200);
+            }
         }
 
         return $this->redirectToRoute('app_tag_index', [], Response::HTTP_SEE_OTHER);
