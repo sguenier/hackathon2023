5e1323ce8a94ce0e8997f784ccfdb316f053661d
mrpinkcat <gatien.anizan@gmail.com>
2023-03-31 12:39:14 +0200

    feat(register): add register

---
+++ frontend/src/components/JajiTag.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+    class="jaji-tag"
+    :class="{
+      'jaji-tag--selected': isSelected,
+    }"
+    @click="$emit('click', value)"
+  >
+    <span class="jaji-tag__label">{{ label }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'JajiTag',
+  emits: [ 'click' ],
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    isSelected: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.jaji-tag {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-blue-action);
+  border: none;
+  border-radius: 8px;
+  color: white;
+  padding: 12px 24px;
+
+  &--selected, &:hover {
+    background-color: var(--color-blue-hover);
+  }
+}
+</style>
