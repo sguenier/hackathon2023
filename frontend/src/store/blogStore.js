35e5630905961a111c204b40abc83f411edc8d49
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-03-31 16:14:55 +0200

    feat(blogstore): add routes getAdvises and GetPost

--- frontend/src/store/blogStore.js
+++ frontend/src/store/blogStore.js
@@ -13,57 +13,7 @@ export const useBlogStore = defineStore('blogStore', {
   actions: {
     async getAdvises() {
       this.isPostsLoading = true;
-      // const { data } = await $API.get('posts/');
-      const data = [
-        {
-          id: 1,
-          title: 'Post 1',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 2,
-          title: 'Post 2',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 3,
-          title: 'Post 3',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 4,
-          title: 'Post 4',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 5,
-          title: 'Post 5',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 6,
-          title: 'Post 6',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 7,
-          title: 'Post 7',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl, vel al',
-        },
-        {
-          id: 8,
-          title: 'Post 8',
-          image: 'https://picsum.photos/200/300',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nunc nisl aliquam nisl',
-        },
-      ];
+      const { data } = await $API.get('admin/posts/');
       await new Promise((resolve) => setTimeout(resolve, 300));
       this.advises = data;
       this.isAdvisesLoading = false;
